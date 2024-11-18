3b55545973ac93a29357336ba69813d4157ec0dc
Viktor-ubuntu-lab <victorprudentelago@outlook.com>
2024-11-18 18:50:44 -0300

    Creating verificator

---
+++ test_runner/file.py
@@ -0,0 +1,32 @@
+import os
+import glob
+
+def read_file(name):
+    if not os.path.isfile(name):
+        raise FileNotFoundError(f"Error: The file '{name}' was not found.")
+    with open(name) as file:
+        data = []
+        for line in file:
+            data.append(int(line.replace("\n", ""), 16))
+    return data, len(data)
+
+def read_files_in_dir(path):
+    try:
+        return [
+            read_file(file)
+            for file in glob.glob(f"{path}/*.hex", recursive=True)
+        ]
+    except FileNotFoundError:
+        print(f"Error: The directory '{path}' was not found.")
+        return []
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+    
+
+# return a list of all the paths with a directory named 'memory' in the path
+def list_find_paths(path):
+    return [
+        os.path.dirname(file)
+        for file in glob.glob(f"{path}/**/memory/*.hex", recursive=True)
+    ]
---
+++ test_runner/verificator.py
@@ -0,0 +1,27 @@
+# Description: Verificator for the processor-ci-tests
+# Enter a path provided in this program and recursively all the tests .hex for each leaf directory create a .xml file with the results 
+# of the tests in that folder the XML files are saved in the test-reports directory with the name 
+# results_{test_path_name}_{timestamp}.xml 
+
+import os
+from processor_ci_communication.core.serial import ProcessorCIInterface
+import test_runner.file as f
+
+class Verificator:
+    def __init__(self, path):
+        self.path = path
+        self.interface = ProcessorCIInterface()
+        self.interface.open()
+
+    def run_tests(self):
+        paths = f.get_all_test_paths(self.path)
+        print(paths)
+
+    
+def main():
+    verifier = Verificator("C:/Users/Usuario/Documents/processor-ci-tests/test_runner")
+    verifier.run_tests()
+    return 0
+
+if __name__ == "__main__":
+    main()
